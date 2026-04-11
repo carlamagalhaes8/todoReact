614ed8a19f74ed46e569969c2daafc28111cf5f6
Carla <carla.magalhaes404@gmail.com>
2026-04-11 19:37:29 -0300

    5.4

--- src/components/ListaTarefas/ListaTarefasItem/ListaTarefasItem.jsx
+++ src/components/ListaTarefas/ListaTarefasItem/ListaTarefasItem.jsx
@@ -3,14 +3,34 @@ import {Botao, TIPO_BOTAO} from '../../Botao';
 
 import { useAppContext } from "../../../hooks";
 
+import { useState } from "react";
+
+import { CampoTexto } from "../../CampoTexto";
+
 const ListaTarefasItem = (props) => {
     const {nome, id} = props;
 
-    const {removerTarefa} = useAppContext();
+    const [estaEditando, setEstaEditando] = useState(false);
+
+    const {editarTarefa, removerTarefa} = useAppContext();
 
     return(
         <li className={styles.listatarefasitem}>
-                {nome}
+
+                {estaEditando && (
+                    <CampoTexto 
+                    defaultValue={nome}
+                    onChange={event => editarTarefa(id, event.currentTarget.value)}
+                    onBlur={() => setEstaEditando(false)} 
+                    autoFocus 
+                    />
+                )}
+                {!estaEditando && (
+                    <span onDoubleClick={() => setEstaEditando(true)}>
+                        {nome}
+                    </span>
+                )}
+
                 <Botao 
                 texto="-" 
                 tipo={TIPO_BOTAO.SECUNDARIO}
